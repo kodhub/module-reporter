ab7e3a425842b74c90c5ae7c02fa9c14880eb158
İsmail Çakır <ismail.cakir@siontek.io>
2020-12-06 22:09:10 +0300

    export types added

--- Block/Adminhtml/Report/GenerateView.php
+++ Block/Adminhtml/Report/GenerateView.php
@@ -1,6 +1,7 @@
 <?php
 namespace Kodhub\Reporter\Block\Adminhtml\Report;
 
+use Kodhub\Reporter\Model\Config\Source\ExportType;
 use Kodhub\Reporter\Model\ReportRepository;
 
 class GenerateView extends \Magento\Backend\Block\Template
@@ -16,16 +17,23 @@ class GenerateView extends \Magento\Backend\Block\Template
     public $reportRepository;
 
     /**
+     * @var ExportType
+     */
+    public $exportType;
+
+    /**
      * GenerateView constructor.
      * @param \Magento\Backend\Block\Template\Context $context
      * @param ReportRepository $reportRepository
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        ReportRepository $reportRepository
+        ReportRepository $reportRepository,
+        ExportType $exportType
     ) {
         parent::__construct($context);
         $this->reportRepository = $reportRepository;
+        $this->exportType = $exportType;
     }
 
     /**
@@ -35,5 +43,13 @@ class GenerateView extends \Magento\Backend\Block\Template
     public function getReportEntity()
     {
         return $this->reportRepository->get($this->getRequest()->getParam('id'));
+    }
+
+    /**
+     * @return array|array[]
+     */
+    public function getExportType()
+    {
+        return $this->exportType->toOptionArray();
     }
 }
